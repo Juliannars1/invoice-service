02856b1e4a33f2a9c3562bb890b5bfda47754b69
Julian Rodriguez <jrodriguez@Julians-MacBook-Pro.local>
2023-07-28 21:16:30 -0500

    finshconfig Dockerfile and Compouse

--- internal/utils/db.go
+++ internal/utils/db.go
@@ -10,7 +10,7 @@ import (
 )
 
 func GetMongoDBConnection() (*mongo.Client, error) {
-	clientOptions := options.Client().ApplyURI("mongodb://localhost:27017")
+	clientOptions := options.Client().ApplyURI("mongodb://mongo:27017")
 
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
