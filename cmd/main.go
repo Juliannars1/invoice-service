98a59c8e9b6709e6c9f5c7c8fab81b819cfeb8ab
Julian Rodriguez <jrodriguez@Julians-MacBook-Pro.local>
2023-07-28 20:32:34 -0500

    main and routes commit

--- cmd/main.go
+++ cmd/main.go
@@ -6,11 +6,34 @@ import (
 	"os"
 	"os/signal"
 	"syscall"
+
+	"github.com/Juliannars1/invoice-service/internal/handlers"
+	"github.com/Juliannars1/invoice-service/internal/repository"
+	"github.com/Juliannars1/invoice-service/internal/service"
+	"github.com/Juliannars1/invoice-service/internal/utils"
+
+	"github.com/gorilla/mux"
 )
 
 func main() {
-	router := routes.setupRoutes()
-	
+	mongoClient, err := utils.GetMongoDBConnection()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer mongoClient.Disconnect(nil)
+
+	invoiceRepo := repository.NewInvoiceRepository(mongoClient, "invoices", "invoice")
+	invoiceService := service.NewInvoiceService(invoiceRepo)
+	invoiceHandler := handlers.NewInvoiceHandler(invoiceService)
+    
+	router := mux.NewRouter()
+	router.HandleFunc("/invoices", invoiceHandler.CreateInvoice).Methods("POST")
+	router.HandleFunc("/invoices", invoiceHandler.GetAllInvoices).Methods("GET")
+	router.HandleFunc("/invoices/{number}", invoiceHandler.GetInvoice).Methods("GET")
+	router.HandleFunc("/invoices/{number}", invoiceHandler.UpdateInvoice).Methods("PUT")
+	router.HandleFunc("/invoices/{number}", invoiceHandler.DeleteInvoice).Methods("DELETE")
+
 	server := &http.Server{
         Addr:    ":8080",
         Handler: router,
